a88afc5ef2b2528d16dbfd84ebfe3a466c1e4db9
Gabriella <gabriboris@gmail.com>
2025-10-14 19:18:21 +0300

    välitallennus
    vittu

---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Attack()
+    {
+        
+    }
+}
