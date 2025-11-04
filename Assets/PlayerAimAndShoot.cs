f30b06dacdffa8af2c81925126cba26134a34be3
Jenjen555 <jennast04@gmail.com>
2025-11-04 20:30:15 +0200

    weapon pyssy
    ionno mään tuun tappelee tänkaa jipii

--- Assets/PlayerAimAndShoot.cs
+++ Assets/PlayerAimAndShoot.cs
@@ -4,46 +4,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
-
 public class PlayerAimAndShoot : MonoBehaviour
 {
+    
     [SerializeField] private GameObject gun;
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform bulletSpawnPoint;
 
     private GameObject bulletInst;
-
     private Vector2 worldPosition;
     private Vector2 direction;
     private float angle;
 
     private void Update()
-    {
+    { 
         HandleGunRotation();
         HandleGunShooting();
     }
-
+    
     private void HandleGunRotation()
     {
         worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         direction = (worldPosition - (Vector2)gun.transform.position).normalized;
         gun.transform.right = direction;
-
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
+        
         Vector3 localScale = new Vector3(1f, 1f, 1f);
-        if (angle > 90 || angle < -90)
-        {
-            localScale.y = -1f;
-        }
-        else
-        {
-            localScale.y = 1f;
-        }
-
-        gun.transform.localScale = localScale;
+        
+        if (angle > 90 || angle < -90) { localScale.y = -1f; }
+        else { localScale.y = 1f; } gun.transform.localScale = localScale;
+        
     }
-
+    
     private void HandleGunShooting()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
--- Assets/Scripts/Weapons/BulletBehavior.cs
+++ Assets/Scripts/Weapons/BulletBehavior.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BulletBehavior : MonoBehaviour
 {
     [SerializeField] private float normalBulletSpeed = 15f;
     [SerializeField] private float destroyTime = 3f;
+    [SerializeField] private LayerMask whatDestroysBullet;
 
     private Rigidbody2D rb;
 
@@ -14,6 +16,14 @@ public class BulletBehavior : MonoBehaviour
         SetDestroyTime();
 
         SetStraightVelocity();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((whatDestroysBullet.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void SetStraightVelocity()
