988b326868ac79f485bef798687ace7a2cffaab3
Jenjen555 <jennast04@gmail.com>
2025-11-14 12:27:11 +0200

    WeaponGun pyssy
    Reloading toimii. 10 ammusta voi ampua kerralla, R painamalla reload, jossa pieni viive, yhteensä käytettävissä 100 luotia.

--- Assets/Scripts/PlayerAimAndShoot.cs
+++ Assets/Scripts/PlayerAimAndShoot.cs
@@ -1,19 +1,41 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerAimAndShoot : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform bulletSpawnPoint;
 
+    [SerializeField] private int maxClipSize = 10;
+    [SerializeField] private int maxTotalAmmo = 100;
+    [SerializeField] private float reloadTime = 1.5f;
+    [SerializeField] private Key reloadKey = Key.R;
+
+    private int currentClipAmmo;
+    private int currentTotalAmmo;
+    private bool isReloading = false;
+
+    private void Awake()
+    {
+        currentClipAmmo = maxClipSize;
+        currentTotalAmmo = maxTotalAmmo;
+    }
+
     private void Update()
     { 
         HandleGunShooting();
+        HandleReloadInput();
     }
 
     private void HandleGunShooting()
     {
+        if (isReloading || currentClipAmmo <= 0)
+        {
+            return;
+        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
@@ -33,6 +55,41 @@ public class PlayerAimAndShoot : MonoBehaviour
             {
                 Physics2D.IgnoreCollision(bulletCollider, playerCollider);
             }
-         }
-     }
+
+            currentClipAmmo--;
+        }
+    }
+
+    private void HandleReloadInput()
+    {
+        if (Keyboard.current[reloadKey].wasPressedThisFrame)
+        {
+            AttemptReload();
+        }
+    }
+
+    public void AttemptReload()
+    {
+        if (isReloading) return;
+
+        if (currentClipAmmo == maxClipSize || currentTotalAmmo == 0) return;
+
+        StartCoroutine(ReloadCoroutine());
+    }
+
+    private IEnumerator ReloadCoroutine()
+    {
+        isReloading = true;
+        
+        yield return new WaitForSeconds(reloadTime);
+
+        int neededAmmo = maxClipSize - currentClipAmmo;
+
+        int ammoToTransfer = Mathf.Min(neededAmmo, currentTotalAmmo);
+
+        currentClipAmmo += ammoToTransfer;
+        currentTotalAmmo -= ammoToTransfer;
+
+        isReloading = false;
+    }
 }
