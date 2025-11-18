b628fd02676a5c639b29b3b61d09f7eff2a8ea10
Gabriella <gabriboris@gmail.com>
2025-11-18 13:40:04 +0200

    ääniii

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,34 +3,39 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private AudioClip Die;
-    private AudioSource audioSource;
+    [SerializeField] private AudioClip[] Hurt;
     public int maxHealth = 10;
     public int health;
+    
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
         health = maxHealth;
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+        if (health > 0 && Hurt.Length > 0)
+    {
+        int randomIndex = Random.Range(0, Hurt.Length);
+        
+        AudioClip randomClip = Hurt[randomIndex];
+
+        AudioSource.PlayClipAtPoint(randomClip, transform.position);
+    }
         if (health <= 0)
         {
-            //audioSource.PlayOneShot(Die);
-            //float soundDuration = Die.length;
             DiePlayer();
         }
     }
     private void DiePlayer()
     {
-        // 1. Play the death sound at the player's position
         AudioSource.PlayClipAtPoint(Die, transform.position);
 
-        // 2. Tell the persistent GameManager to end the game and show the UI
         if (GameManager.Instance != null)
         {
             GameManager.Instance.EndGame();
         }
+        Destroy(gameObject);
     }
 }
